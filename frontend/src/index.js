e808a1a5f946eb1e4e9706ebfff582178378b090
Reid <reid.t.sata@gmail.com>
2018-06-16 18:04:45 -0700

    add redux dev tools

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,7 +6,10 @@ import rootReducer from './reducers';
 
 import registerServiceWorker from './registerServiceWorker';
 
-const store = createStore(rootReducer);
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 render(
   <Root store={store} />,
